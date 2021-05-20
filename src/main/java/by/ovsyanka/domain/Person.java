5e8eb0b7da0a4f32e59def1cbe65c0ffbb9fdaee
iamovsyanka <anakstkv@gmail.com>
2021-05-20 17:09:44 +0300

    feat: add floors to person and test

--- src/main/java/by/ovsyanka/domain/Person.java
+++ src/main/java/by/ovsyanka/domain/Person.java
@@ -3,6 +3,7 @@ package by.ovsyanka.domain;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+
 import static com.google.common.base.Preconditions.checkArgument;
 
 import java.util.UUID;
@@ -18,14 +19,30 @@ public class Person {
     private final int minWeight = 20;
     private final int maxWeight = 150;
 
-    public Person(int weight) {
-        checkArgument(weight >= minWeight && weight <= maxWeight,  String.format("Weight must be between %s and %s", minWeight, maxWeight));
+    private final int maxFloor;
+    @Getter
+    private final int currentFloor;
+    @Getter
+    private final int selectedFloor;
+
+    public Person(int weight, int maxFloor, int currentFloor, int selectedFloor) {
+        checkArgument(weight >= minWeight && weight <= maxWeight,
+                String.format("Weight must be between %s and %s", minWeight, maxWeight));
+        checkArgument(currentFloor <= maxFloor && currentFloor >= 1,
+                String.format("Current floor must be less than %s and more than 0", maxFloor + 1));
+        checkArgument(currentFloor != selectedFloor,
+                "Selected floor cannot be equal to the current one");
+        checkArgument(selectedFloor <= maxFloor && selectedFloor >= 1,
+                "Selected floor must be less than %s and more than 0", maxFloor + 1);
 
         this.id = UUID.randomUUID();
         this.weight = weight;
+        this.maxFloor = maxFloor;
+        this.currentFloor = currentFloor;
+        this.selectedFloor = selectedFloor;
     }
 
-    public static Person of(int weight) {
-        return new Person(weight);
+    public static Person of(int weight, int maxFloor, int currentFloor, int selectedFloor) {
+        return new Person(weight, maxFloor, currentFloor, selectedFloor);
     }
 }
--- src/test/java/by/ovsyanka/domain/PersonTest.java
+++ src/test/java/by/ovsyanka/domain/PersonTest.java
@@ -8,9 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonTest {
 
+    private final int weight = 50;
+    private final int maxFloor = 10;
+    private final int currentFloor = 3;
+    private final int selectedFloor = 7;
+
     @Test
     public void testCreatePerson() {
-        Person person = Person.of(50);
+        Person person = Person.of(weight, maxFloor, currentFloor, selectedFloor);
 
         assertNotNull(person);
     }
@@ -19,35 +24,96 @@ public class PersonTest {
     public void testCreateRandomPerson() {
         for (int i = 0; i < 5; i++) {
             int randomWeight = new Random().ints(20, 150).findFirst().getAsInt();
-            assertDoesNotThrow(() -> Person.of(randomWeight));
+            assertDoesNotThrow(() -> Person.of(randomWeight, maxFloor, currentFloor, selectedFloor));
         }
     }
 
     @Test
     public void testGetWeight() {
-        Person person = Person.of(50);
+        Person person = Person.of(weight, maxFloor, currentFloor, selectedFloor);
 
-        assertEquals(50, person.getWeight());
+        assertEquals(weight, person.getWeight());
     }
 
     @Test
     public void testInvalidWeight() {
         //less than min
-        assertThrows(IllegalArgumentException.class, () -> Person.of(19));
-        assertThrows(IllegalArgumentException.class, () -> Person.of(-10));
+        assertThrows(IllegalArgumentException.class, () -> Person.of(19, maxFloor, currentFloor, selectedFloor));
+        assertThrows(IllegalArgumentException.class, () -> Person.of(-10, maxFloor, currentFloor, selectedFloor));
 
         //more than max
-        assertThrows(IllegalArgumentException.class, () -> Person.of(151));
-        assertThrows(IllegalArgumentException.class, () -> Person.of(200));
+        assertThrows(IllegalArgumentException.class, () -> Person.of(151, maxFloor, currentFloor, selectedFloor));
+        assertThrows(IllegalArgumentException.class, () -> Person.of(200, maxFloor, currentFloor, selectedFloor));
     }
 
     @Test
     public void testMinWeight() {
-        assertDoesNotThrow(() -> Person.of(20));
+        assertDoesNotThrow(() -> Person.of(20, maxFloor, currentFloor, selectedFloor));
     }
 
     @Test
     public void testMaxWeight() {
-        assertDoesNotThrow(() -> Person.of(150));
+        assertDoesNotThrow(() -> Person.of(150, maxFloor, currentFloor, selectedFloor));
+    }
+
+    @Test
+    public void testGetCurrentFloor() {
+        Person person = Person.of(weight, maxFloor, currentFloor, selectedFloor);
+
+        assertEquals(currentFloor, person.getCurrentFloor());
+    }
+
+    @Test
+    public void testMaxCurrentFloor() {
+        assertDoesNotThrow(() -> Person.of(weight, maxFloor, maxFloor, selectedFloor));
+    }
+
+    @Test
+    public void testMinCurrentFloor() {
+        assertDoesNotThrow(() -> Person.of(weight, maxFloor, 1, selectedFloor));
+    }
+
+    @Test
+    public void testInvalidCurrentFloor() {
+        //less than 1
+        assertThrows(IllegalArgumentException.class, () -> Person.of(weight, maxFloor, 0, selectedFloor));
+        assertThrows(IllegalArgumentException.class, () -> Person.of(weight, maxFloor, -10, selectedFloor));
+
+        //more than max
+        assertThrows(IllegalArgumentException.class, () -> Person.of(weight, maxFloor, maxFloor + 1, selectedFloor));
+        assertThrows(IllegalArgumentException.class, () -> Person.of(weight, maxFloor, maxFloor + 100, selectedFloor));
+    }
+
+    @Test
+    public void testGetSelectedFloor() {
+        Person person = Person.of(weight, maxFloor, currentFloor, selectedFloor);
+
+        assertEquals(selectedFloor, person.getSelectedFloor());
+    }
+
+    @Test
+    public void testMaxSelectedFloor() {
+        assertDoesNotThrow(() -> Person.of(weight, maxFloor, currentFloor, maxFloor));
+    }
+
+    @Test
+    public void testMinSelectedFloor() {
+        assertDoesNotThrow(() -> Person.of(weight, maxFloor, currentFloor, 1));
+    }
+
+    @Test
+    public void testInvalidSelectedFloor() {
+        //less than 1
+        assertThrows(IllegalArgumentException.class, () -> Person.of(weight, maxFloor, currentFloor, 0));
+        assertThrows(IllegalArgumentException.class, () -> Person.of(weight, maxFloor, currentFloor, -10));
+
+        //more than max
+        assertThrows(IllegalArgumentException.class, () -> Person.of(weight, maxFloor, currentFloor, maxFloor + 1));
+        assertThrows(IllegalArgumentException.class, () -> Person.of(weight, maxFloor, currentFloor, maxFloor + 100));
+    }
+
+    @Test
+    public void testSelectCurrentFloor() {
+        assertThrows(IllegalArgumentException.class, () -> Person.of(weight, maxFloor, currentFloor, currentFloor));
     }
 }
